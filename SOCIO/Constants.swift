d12ebac5f6f4eef7fb1f1048da2ee6b3d3541db0
17hdfied <hardikwason@gmail.com>
2017-10-07 17:44:51 +0530

    Sign-in-ui-custom completed

---
+++ SOCIO/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SOCIO
+//
+//  Created by Hardik Wason on 07/10/17.
+//  Copyright © 2017 Hardik Wason. All rights reserved.
+//
+
+import UIKit
+
+let color_gray : CGFloat = 155.0 / 255.0

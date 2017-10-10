d3944e23dfef28f86382c1206b3b400984ae7fb8
17hdfied <hardikwason@gmail.com>
2017-10-10 23:32:55 +0530

    Picker-image-done

--- SOCIO/Feed.swift
+++ SOCIO/Feed.swift
@@ -10,13 +10,21 @@ import UIKit
 import SwiftKeychainWrapper
 import Firebase
 
-class Feed: UIViewController,UITableViewDelegate,UITableViewDataSource {
+class Feed: UIViewController,UITableViewDelegate,UITableViewDataSource,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     @IBOutlet weak var tableview: UITableView!
-var posts = [Post]()
+    @IBOutlet weak var addimg: CircleView!
+    
+        var posts = [Post]()
+    var imagePicker: UIImagePickerController!
     override func viewDidLoad() {
         super.viewDidLoad()
-           tableview.delegate = self
+        tableview.delegate = self
         tableview.dataSource = self
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+      
         
         DataServices.ds.REF_POSTS.observe(.value, with: { (snapshot) in
             self.posts = []
@@ -35,8 +43,29 @@ var posts = [Post]()
             self.tableview.reloadData()
         })
     }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            addimg.image = image
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func add_imagetap(_ sender: Any) {
+       present(imagePicker,animated: true,completion: nil)
+        
+        
+        
+    }
+    
+    
 
-  
+    
+    
+    
+    
+    
+    
+    
     @IBAction func logout_btn(_ sender: Any) {
         let keychainResult = KeychainWrapper.standard.removeObject(forKey: Key_chain)
         print("HD: ID removed from keychain \(keychainResult)")

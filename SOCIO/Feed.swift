a3baab9980cdaa23fb4830027cdc1867b2ad88ec
17hdfied <hardikwason@gmail.com>
2017-10-10 21:35:44 +0530

    Firebase get complete

--- SOCIO/Feed.swift
+++ SOCIO/Feed.swift
@@ -12,14 +12,27 @@ import Firebase
 
 class Feed: UIViewController,UITableViewDelegate,UITableViewDataSource {
     @IBOutlet weak var tableview: UITableView!
-
+var posts = [Post]()
     override func viewDidLoad() {
         super.viewDidLoad()
            tableview.delegate = self
         tableview.dataSource = self
         
         DataServices.ds.REF_POSTS.observe(.value, with: { (snapshot) in
-            print(snapshot.value)
+            self.posts = []
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot]
+            {
+                for snap in snapshot {
+                    print("SNAP:\(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject>
+                    {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableview.reloadData()
         })
     }
 
@@ -36,11 +49,14 @@ class Feed: UIViewController,UITableViewDelegate,UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        
+        return posts.count
     
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let post = posts[indexPath.row]
+        print("HD: The caption will be \(post.caption)")
         return tableview.dequeueReusableCell(withIdentifier: "postcell") as! PostCell
     }
    
---
+++ SOCIO/Post.swift
@@ -0,0 +1,58 @@
+//
+//  Post.swift
+//  SOCIO
+//
+//  Created by Hardik Wason on 10/10/17.
+//  Copyright © 2017 Hardik Wason. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    private var _caption : String!
+    private var _imageURL : String!
+    private var _likes : Int!
+    private var _postKey : String!
+    
+    var caption : String
+    {
+        return _caption
+    }
+    var imageURL : String
+    {
+        return _imageURL
+    }
+    var likes : Int
+    {
+        return _likes
+    }
+    var postKey : String
+    {
+        return _postKey
+    }
+    
+    init(caption: String, imageURL: String, likes: Int)
+    {
+        self._caption = caption
+        self._imageURL = imageURL
+        self._likes = likes
+    }
+    init(postKey: String, postData: Dictionary<String, AnyObject>)
+    {
+        self._postKey = postKey
+        if let caption = postData["caption"]
+        {
+            self._caption = caption as? String
+        }
+        if let imageURL = postData["imageURL"] as? String
+        {
+            self._imageURL = imageURL
+        }
+        if let likes = postData["likes"] as? Int
+        {
+            self._likes = likes
+        }
+
+        
+    }
+}
